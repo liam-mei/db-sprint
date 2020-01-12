38a6306603674013343e623d89caa8f142225e5e
Derrick Mei <derrick.mei.168@gmail.com>
2020-01-12 02:50:46 -0600

    getResourcesById
    also by projectId for stretch

--- data/dataModel.js
+++ data/dataModel.js
@@ -14,6 +14,32 @@ function getAllTasks() {
     .select("t.*", "p.name as project_name", "p.description as project_description");
 }
 
+function getResourceById(id) {
+  return db("resources")
+    .where({ id })
+    .first();
+}
+
+function getProjectById(id) {
+  return db("projects")
+    .where({ id })
+    .first();
+}
+
+function getTasksById(id) {
+  return db("tasks as t")
+    .join("projects as p", "t.project_id", "p.id")
+    .select("t.*", "p.name as project_name", "p.description as project_description")
+    .where({ id });
+}
+
+function getTasksByProjectId(project_id) {
+  return db("tasks").where({ project_id });
+}
+
+function getResourcesByProjectId(project_id) {
+  return db("resources").where({ project_id });
+}
 
 
 exports.module = {
