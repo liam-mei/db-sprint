449b174ecd9489201d6c340d3801ca77481273e7
Derrick Mei <derrick.mei.168@gmail.com>
2020-01-12 04:36:50 -0600

    fixed dbModel
    tasks and projects now return completed bool
    
    nested data query finished

--- data/dataModel.js
+++ data/dataModel.js
@@ -1,17 +1,20 @@
 const db = require("./dbConfig");
+const helpers = require("./helpers");
 
 function getAllResources() {
   return db("resources");
 }
 
-function getAllProjects() {
-  return db("projects");
+async function getAllProjects() {
+  const projects = await db("projects");
+  return helpers.convertArrayToBool(projects);
 }
 
-function getAllTasks() {
-  return db("tasks as t")
+async function getAllTasks() {
+  const tasks = await db("tasks as t")
     .join("projects as p", "t.project_id", "p.id")
     .select("t.*", "p.name as project_name", "p.description as project_description");
+  return helpers.convertArrayToBool(tasks);
 }
 
 function getResourceById(id) {
@@ -20,17 +23,20 @@ function getResourceById(id) {
     .first();
 }
 
-function getProjectById(id) {
-  return db("projects")
+async function getProjectById(id) {
+  const data = await db("projects")
     .where({ id })
     .first();
+  return helpers.convertObjToBool(data);
 }
 
-function getTasksById(id) {
-  return db("tasks as t")
+async function getTasksById(id) {
+  const data = await db("tasks as t")
     .join("projects as p", "t.project_id", "p.id")
     .select("t.*", "p.name as project_name", "p.description as project_description")
-    .where({ id });
+    .where({ "t.id": id })
+    .first();
+  return helpers.convertObjToBool(data);
 }
 
 function getTasksByProjectId(project_id) {
@@ -38,7 +44,25 @@ function getTasksByProjectId(project_id) {
 }
 
 function getResourcesByProjectId(project_id) {
-  return db("resources").where({ project_id });
+  return db("projects_resources as pr")
+    .where({ "pr.project_id": project_id })
+    .join("resources as r", "r.id", "pr.resource_id")
+    .select('r.*');
+}
+
+async function getCompleteProjectById(project_id) {
+  const promiseArray = [
+    getProjectById(project_id),
+    getResourcesByProjectId(project_id),
+    getTasksByProjectId(project_id)
+  ];
+
+  const [resolvedProject, resolvedResources, resolvedTasks] = await Promise.all(promiseArray);
+
+  const boolProject = helpers.convertObjToBool(resolvedProject)
+  const boolTasks = helpers.convertArrayToBool(resolvedTasks);
+
+  return { ...boolProject, resources: resolvedResources, tasks: boolTasks };
 }
 
 async function addProject(project) {
@@ -56,7 +80,7 @@ async function addTask(task) {
   return getTasksById(id);
 }
 
-exports.module = {
+module.exports = {
   // MVP
   getAllResources,
   getAllProjects,
@@ -68,6 +92,8 @@ exports.module = {
 
   getTasksByProjectId,
   getResourcesByProjectId,
+
+  getCompleteProjectById,
 
   addProject,
   addResource,
